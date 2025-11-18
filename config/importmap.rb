c395d366eb8bd55d422b1028833fcb7dbab8fb6f
oliver mueller <omue72@gmx.de>
2025-11-18 13:23:28 +0100

    added pin utils and config manually followed by pin_all_from controllers

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,7 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "utils/message_animations", to: "utils/message_animations.js"
 pin "utils/slide_animations", to: "utils/slide_animations.js"
 pin "config/animation_constants", to: "config/animation_constants.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "gsap", to: "https://cdn.jsdelivr.net/npm/gsap@3.13.0/+esm"
