ca4733a03a749bc55ac512b9796bf1b8a2b57217
oliver mueller <omue72@gmx.de>
2025-11-18 13:12:07 +0100

    changed pin in importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,9 +4,9 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/utils", under: "utils"
-pin_all_from "app/javascript/config", under: "config"
+pin "utils/message_animations", to: "utils/message_animations.js"
+pin "utils/slide_animations", to: "utils/slide_animations.js"
+pin "config/animation_constants", to: "config/animation_constants.js"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "gsap", to: "https://cdn.jsdelivr.net/npm/gsap@3.13.0/+esm"
