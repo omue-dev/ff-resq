8d326cac6836aadbd76bbd858077940448c3ae1a
oliver mueller <omue72@gmx.de>
2025-11-29 22:39:39 +0100

    force asset rebuild

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = "1.0"
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w(bootstrap.min.js popper.js fox-avatar-min.png)
+# force rebuild Sat Nov 29 10:39:39 PM CET 2025
